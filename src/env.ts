f5756f5614501c36b578a982287de0881918d824
Sonny <sonnyelvis@hotmail.co.uk>
2023-12-23 20:10:10 +0000

    build: expose zip env.

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,4 @@ import 'dotenv/config';
 export const BROWSER = process.env.BROWSER === 'firefox' ? 'firefox' : 'chrome';
 export const GECKO_ID = process.env.GECKO_ID ?? '123qwe456asd789zxc';
 export const PRODUCTION = process.env.NODE_ENV === 'production';
+export const ZIP = process.env.ZIP === 'true' ? true : false;
