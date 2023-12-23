bf895c757d2a32397fecca6e51c0823077ed3967
Sonny <sonnyelvis@hotmail.co.uk>
2023-12-23 20:10:51 +0000

    feat: adds watch vod button to clips that are accessed through kick app rather than direct link.

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,7 +2,7 @@ function sendMessage(tabID: number, message: {}, callback: ((res: any) => void))
     try {
         const res = chrome.tabs.sendMessage(tabID, message, (res) => {
             if (chrome.runtime.lastError) {
-                console.warn('Callback failed, tab likely didn\'t exist', chrome.runtime.lastError.message);
+                console.warn('Callback failed, tab probably does not exist', chrome.runtime.lastError.message);
             }
             callback(res);
         });
@@ -27,16 +27,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const url = new URL(tab.url ?? 'http://a');
     if (url.hostname.includes('kick.com')) {
         if (changeInfo.status === 'complete') {
-            if (url.searchParams.has('clip')) {
+            if (url.pathname.includes('/video') && url.searchParams.has('t')) {
+                sendMessage(tabId, {
+                    message: 'video', 
+                    time: url.searchParams.get('t')
+                }, (res: any) => {});
+            } else if (url.searchParams.has('clip')) {
                 sendMessage(tabId, {
                     message: 'clip', 
                     username: url.pathname.replace('/', ''), 
                     clipID: url.searchParams.get('clip')
                 }, (res: any) => {});
-            } else if (url.pathname.includes('/video') && url.searchParams.has('t')) {
+            } else {
                 sendMessage(tabId, {
-                    message: 'video', 
-                    time: url.searchParams.get('t')
+                    message: 'general',
+                    username: url.pathname.replace('/', ''),
                 }, (res: any) => {});
             }
         }
--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -16,35 +16,63 @@ async function getData(clipID: string, username: string) {
     return { userData, clipData };
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'clip') {
-        getData(request.clipID, request.username).then(({ userData, clipData }) => {
-            const stream = userData.previous_livestreams.find((stream: any) => stream.id === Number(clipData.clip.livestream_id));
-            if (stream !== undefined) {
-                const streamStarted = new Date(stream.start_time);
-                const clipStarted = new Date(clipData.clip.started_at);
-                // clipStart: clipEnd - clipData.clip.duration
-                const clipEnd = Math.trunc((clipStarted.valueOf() - streamStarted.valueOf()) / 1000);
-                const downloadButton = Array.from(document.querySelectorAll('div.inner-label')).find(ele => ele.textContent === 'Download');
-                const vDataAttribute = downloadButton?.getAttributeNames().find(el => el.startsWith('data-v'));
-                const parentContainer = downloadButton?.closest('.flex.items-center');
-                const vodHTML = `
-                    <a ${vDataAttribute} class="variant-highlight size-md base-button" href="https://kick.com/video/${stream.video.uuid}?t=${clipEnd}"> 
-                        <div ${vDataAttribute} class="button-content"> 
-                            <div ${vDataAttribute} class="inner-label">Watch vod</div>
-                        </div>
-                    </a>`;
-                if (parentContainer) {
-                    parentContainer.insertAdjacentHTML('beforeend', vodHTML);
-                }
-            }
-        });
-        sendResponse({ message: 'clip' });
-        return true;
+function createButton(userData: any, clipData: any) {
+    const stream = userData.previous_livestreams.find((stream: any) => stream.id === Number(clipData.clip.livestream_id));
+    if (stream !== undefined) {
+        const streamStarted = new Date(stream.start_time);
+        const clipStarted = new Date(clipData.clip.started_at);
+        // clipStart: clipEnd - clipData.clip.duration
+        const clipEnd = Math.trunc((clipStarted.valueOf() - streamStarted.valueOf()) / 1000);
+        const downloadButton = Array.from(document.querySelectorAll('div.inner-label')).find(ele => ele.textContent === 'Download');
+        const vDataAttribute = downloadButton?.getAttributeNames().find(el => el.startsWith('data-v'));
+        const parentContainer = downloadButton?.closest('.flex.items-center');
+        const vodHTML = `
+            <a ${vDataAttribute} class="variant-highlight size-md base-button" href="https://kick.com/video/${stream.video.uuid}?t=${clipEnd}"> 
+                <div ${vDataAttribute} class="button-content"> 
+                    <div ${vDataAttribute} class="inner-label">Watch vod</div>
+                </div>
+            </a>`;
+        if (parentContainer) {
+            parentContainer.insertAdjacentHTML('beforeend', vodHTML);
+        }
     }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'video') {
         document.dispatchEvent(new CustomEvent('KICKY_GET_TIME', { detail: request.time }));
         sendResponse({ message: 'video' });
         return true;
-    };
+    } else if (request.message === 'clip') {
+        getData(request.clipID, request.username).then(({ userData, clipData }) => {
+            createButton(userData, clipData); 
+        });
+        sendResponse({ message: 'clip' });
+        return true;
+    } else if (request.message === 'general') {
+        const clipsSeen: string[] = [];
+        const observer = new MutationObserver((changes) => {
+            changes.forEach((change) => {
+                if (change.target instanceof HTMLDivElement && change.target.id === 'clip-video-player') {
+                    const clipID = (change.target.getAttribute('poster') ?? '').split('/')[5];
+                    if (!clipsSeen.includes(clipID)) {
+                        clipsSeen.push(clipID);
+                        getData(clipID, request.username).then(({ userData, clipData }) => {
+                            createButton(userData, clipData); 
+                        });
+                    }
+                }
+                change.removedNodes.forEach((node) => {
+                    if (node instanceof HTMLDivElement && node.id === 'clip-video-player') {
+                        const clipID = (node.getAttribute('poster') ?? '').split('/')[5];
+                        const indexID = clipsSeen.indexOf(clipID);
+                        clipsSeen.splice(indexID, indexID !== -1 ? 1 : 0);
+                    }
+                });
+            });
+        }); 
+        observer.observe(document.body, { subtree: true, childList: true });
+        sendResponse({ message: 'general' });
+        return true;
+    }
 });
